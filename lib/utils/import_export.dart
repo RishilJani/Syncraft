7bdc7371aeb82dd3528fb200e52e43f471af31c1
RishilJani <rishilj9108@gmail.com>
2025-07-18 17:58:25 +0530

    Adding utils and registration directory

---
+++ lib/utils/import_export.dart
@@ -0,0 +1,6 @@
+export 'package:flutter/material.dart';
+export 'package:flutter/cupertino.dart' hide RefreshCallback;
+export 'package:get/get.dart';
+export 'package:syncraft/registration/registration_model.dart';
+export 'package:syncraft/registration/registration_controller.dart';
+export 'package:syncraft/registration/registration_view.dart';
