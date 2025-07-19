fac4effaa767c406744ade1cd6329ae2e3f0312d
RishilJani <rishilj9108@gmail.com>
2025-07-19 16:42:33 +0530

    completed login & signup

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: RT_LOGIN_REGISTRATION_PAGE,
     );
   }
 }
@@ -40,18 +40,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
---
+++ lib/utils/string_constants.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: constant_identifier_names
+// region Routes
+const String RT_LOGIN_REGISTRATION_PAGE = "/registration_login";
+const String RT_TEMP_PAGE = "/Temp";
+// endregion
+
+// region User
+const String USER_EMAIL = 'email';
+const String USER_PASSWORD = 'password';
+const String USER_ROLE = 'role';
+// endregion
+
+// region Button
+const String BTN_LOGIN = "Login";
+const String BTN_SIGNUP = "Sign Up";
+// endregion
+
+const String BASE_URL = 'https://syncraft-api-server.onrender.com';
