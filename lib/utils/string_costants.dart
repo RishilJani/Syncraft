c0ffa2e61f8ae7ee304176496fb1656aa3ac0795
KaranLukka <Karanlukka77@gmail.com>
2025-07-19 15:00:00 +0530

    Profile card completed

---
+++ lib/utils/string_costants.dart
@@ -0,0 +1,8 @@
+//region UTILS CONSTANTS
+const String ROBOTO = "Roboto";
+const String Nunito = "Nunito";
+//endregion
+
+//region APPBAR CONSTANTS
+const String APPBAR_TITLE = "Welcome to Syncraft";
+//endregion
