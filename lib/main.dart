be71bf1e2d2cd2b552655ae1540595532fb675c0
FainNovity <23010101189@darshan.ac.in>
2025-07-19 12:32:06 +0530

    Member dashboard screen + member modal and list card

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:syncraft/member/dashboard/dashboard_view.dart';
 import 'package:syncraft/utils/import_export.dart';
 
 void main() {
@@ -16,59 +17,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      // home: MemberDashboardView(memberId: 1,memberName: "Harsh", teamName: "Tech wizards", currentProject: MemberProject(id: 17, name: "Hackathon", description: "Converstech", totalTasks: 10, completedTasks: 5),),
+      home: MemberDashboardView(memberId: 1, memberName: "Harsh",),
     );
   }
 }
